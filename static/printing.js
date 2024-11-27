4866a36d1060e6335f282391c7d579bb5e027e7a
unknown <pandeyabhinav2045@gmail.com>
2024-11-27 17:47:28 +0530

    after some changes

---
+++ static/printing.js
@@ -0,0 +1,18 @@
+let element = document.getElementById('output')
+console.log("hello this is working fine ");
+console.log(document.querySelector('#output'));
+console.log(element);
+// element.classList.add("outputbox");
+
+// element = document.getElementById("output");
+// element.addEventListener("input", () => {
+//     element.style.height = "auto";
+//     element.style.height = `${element.scrollHeight}px`;
+// });
+
+// const adjustHeight = () => {
+//     element.style.height = "auto";
+//     element.style.height = `${element.scrollHeight}px`; // Adjust height to content
+// };
+// element.addEventListener("input", adjustHeight);
+// adjustHeight();
